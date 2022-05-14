f1d3743bf94daff528eb2b80863b177d8f224d51
Crucix <fchang1720@gmail.com>
2022-05-14 14:54:23 -0500

    filled in user model and index

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String, 
+            unique: true,
+            required: true
+        },
+
+        email: {
+            type: String,
+            required: true,
+            match: /.+\@.+\..+/,
+            unique: true
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought',
+            },
+        ],
+
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,  
+    }
+
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
